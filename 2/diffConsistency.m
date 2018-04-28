758a9c99c7cc4109c9bff8d8881fef1f762e9788
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2018-04-28 16:43:29 +0200

    Created functions for the second exercise

--- 2/diffConsistency.m
+++ 2/diffConsistency.m
@@ -8,3 +8,10 @@
 % errList   array containing the errors in approximating df
 % hList     array containing values of h
 function [errList, hList] = diffConsistency(f, df, x, iMax, h0)
+  for i = [1,iMax]
+    hList(i) = h0 * 10^(-i-1); 
+    df_approx = (f(x+hList(i)) - f(x))/hList(i);
+    errList(i) = abs(df_approx - df(x));
+  end
+
+end 
