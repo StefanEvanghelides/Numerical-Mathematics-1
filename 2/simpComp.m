f5d71f7501d0789d729adae1d219bbb1139407c7
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2018-04-29 11:16:08 +0200

    Worked some more on Numerical Integration
    Still stuck on composite Simpson's, for some reason the method does not converge.

--- 2/simpComp.m
+++ 2/simpComp.m
@@ -2,8 +2,7 @@ function [int, flag, stats] = simpComp(f, a, b, tol, hMin)
     % Initialise variables
     h = b - a;
     n = 1;
-    x_mean = (a+b)/2;
-    int = h / 6 * (f(a) + f(b)) + h / 3 * x_mean;
+    int = (h / 3) * (f(a) + f(b));
     flag = 1;
 
     if nargout == 3
@@ -18,15 +17,15 @@ function [int, flag, stats] = simpComp(f, a, b, tol, hMin)
         end
 
         % Update the integral with the new nodes
-        intNew = int / 2;
+        intNew = int;
         for j = 1 : 2 : n
-            mean_interval = (j * (j + 1))/2;
-            intNew = intNew + h / 3 * (f(a + j * h) + 2 * f(a + mean_interval * h));
+            intNew = intNew + ((2 * h) / 3) * f(a + (j+1) * h) + ...
+                ((4 * h) / 3) * f(a + j*h) ;
         end
 
         % Estimate the error
+        int = intNew / 2;
         errorEst = 1 / 3 * (int - intNew);
-        int = intNew;
         if nargout == 3 % Update stats
             stats.totalErEst = [stats.totalErEst; abs(errorEst)];
             stats.totalNrIntervals = [stats.totalNrIntervals; n / 2];
@@ -37,5 +36,3 @@ function [int, flag, stats] = simpComp(f, a, b, tol, hMin)
             break
         end
     end
-   
-end
