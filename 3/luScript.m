7928c98b7e39e018ca400df63c608b959706fa78
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2018-05-08 22:45:56 +0200

    started working on a script for LU

--- 3/luScript.m
+++ 3/luScript.m
@@ -4,3 +4,12 @@ x = rand(10,1);
 % calculate b
 b = A*x;
 c_est = luNaive(A,b);
+
+for i = 1:16
+    epsilon(i) = 10^-i;
+    rel_err(i) = norm(c - c_est)/norm(c);
+    cond(A_eps,2);
+    cond(L_eps,2);
+    cond(U_eps,2);
+    cond(A_eps - L_eps*U_eps,'fro');
+end
