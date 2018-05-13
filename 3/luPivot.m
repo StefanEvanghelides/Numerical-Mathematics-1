3ce6ad2792846ab283f98ecd2cbfb2fc476ad140
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2018-05-13 03:51:54 +0200

    Initial commit for luPivot

---
+++ 3/luPivot.m
@@ -0,0 +1,30 @@
+% INPUT
+% A         square matrix
+% b         right hand side
+% OUTPUT
+% x         solution such that A*x=b
+% L         lower triangular matrix such that A = L*U
+% U         upper triangular matrix such that A = L*U
+function [x, L, U, P] = luPivot(A, b)
+    % Computing L and U
+    n = length(A);
+    P = eye(n); % Identity matrix
+    L = zeros(n);
+    U = A;
+    for k = 1 : n
+        % Find r_hat such that |U(r_hat,k)| = max (r=k,...,n) |U(r,k)|
+        % Swap r_hat-th row with the r-th row in L, U and P
+        
+        for i = k+1 : n
+            L(i,k) = U(i,k)/U(k,k);
+            for j = k : n
+               U(i,j) = U(i,j) - L(i,k) * U(k,j);
+            end
+        end
+    end
+    L(k,k) = 1;
+    
+    % Computing x based on L, U and b:
+    y = L \ b;
+    x = U \ y;
+end
