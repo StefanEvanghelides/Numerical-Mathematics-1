6bf325e693d2371e2e997eb4d504b09589e8ef51
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2018-05-16 13:52:34 +0200

    Completed second part of the lab(2)

--- 4/powerMethods/MAIN_powerMethod.m
+++ 4/powerMethods/MAIN_powerMethod.m
@@ -9,16 +9,18 @@ maxIt = 10^3;
 [lambda, x, flag, lambdaHist, convHist] = powerMethod(A, x0, tol, maxIt);
 
 rel_err = abs(lambda - lambdaHist) / abs(lambda);
-size_lambda_hist = size(lambdaHist,2)
+iterations = size(lambdaHist,2)
 flag
 lambda
-lambdaHist(size_lambda_hist)
-convHist(size_lambda_hist)
+lambdaHist(iterations)
+convHist(iterations)
 
-loglog(1:size_lambda_hist, lambdaHist, 'b.-', 'DisplayName', '\lambda^{(K)}'); hold on
-loglog(1:size_lambda_hist, convHist, 'r', 'DisplayName', 'error estimate')
+loglog(1:iterations, lambdaHist, 'b.-', 'DisplayName', '\lambda^{(K)}'); hold on
+loglog(1:iterations, convHist, 'r', 'DisplayName', 'error estimate')
 line([1 maxIt],[lambda lambda], 'Color', 'g', 'DisplayName', '\lambda_1')
 hold off;
 %ylim([-inf lambda*1.1]); % Increase y-azis space by 10%
 title('Lambda approximation');
+xlabel('Iteration steps');
+ylabel('Value');
 legend('Location', 'best');
