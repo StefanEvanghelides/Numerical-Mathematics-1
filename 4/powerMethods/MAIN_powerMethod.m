8d5923dbf81808cc1e5800a5de7bed90513b1a2b
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2018-05-16 16:58:34 +0200

    Update on the second part based on TA

--- 4/powerMethods/MAIN_powerMethod.m
+++ 4/powerMethods/MAIN_powerMethod.m
@@ -6,21 +6,35 @@ x0 = rand(N^2, 1);
 tol = 10^-12;
 maxIt = 10^3;
 
-[lambda, x, flag, lambdaHist, convHist] = powerMethod(A, x0, tol, maxIt);
+% Computing B
+P = diag(diag(A));
+lambda_max = eigs(A,1);
+lambda_min = eigs(A,1,'SM');
+alpha_opt = 2 / (lambda_max + lambda_min);
+alpha = 0.5;
 
+B = eye(N^2) - alpha_opt * P\A;
+
+% Running the program
+[lambda, x, flag, lambdaHist, convHist] = powerMethod(B, x0, tol, maxIt);
+lambda = abs(lambda);
+lambdaHist = abs(lambdaHist);
 rel_err = abs(lambda - lambdaHist) / abs(lambda);
-iterations = size(lambdaHist,2)
-flag
-lambda
-lambdaHist(iterations)
-convHist(iterations)
+iterations = size(lambdaHist,2);
 
-loglog(1:iterations, lambdaHist, 'b.-', 'DisplayName', '\lambda^{(K)}'); hold on
-loglog(1:iterations, convHist, 'r', 'DisplayName', 'error estimate')
-line([1 maxIt],[lambda lambda], 'Color', 'g', 'DisplayName', '\lambda_1')
+
+% Plotting 
+
+p1 = loglog(1:iterations, lambdaHist, 'b.-', 'DisplayName', '\lambda^{(K)}'); hold on
+%p2 = loglog(1:iterations, rel_err, 'r', 'DisplayName', 'error estimate');
+p3 = line([1 maxIt],[lambda lambda], 'Color', 'g', 'DisplayName', '\lambda_1');
 hold off;
-%ylim([-inf lambda*1.1]); % Increase y-azis space by 10%
+if lambda < 0
+    ylim([lambda*1.3 lambdaHist(1)*0.8]);
+else
+    ylim([lambdaHist(1)*0.8 lambda*1.3]);
+end
 title('Lambda approximation');
 xlabel('Iteration steps');
 ylabel('Value');
-legend('Location', 'best');
+legend([p3 p1], 'Location', 'best');
--- 4/powerMethods/powerMethod.m
+++ 4/powerMethods/powerMethod.m
@@ -10,17 +10,7 @@
 % lambdaHist    approximate eigenvalue per iteration
 % convHist      error estimate per iteration
 function [lambda,x,flag,lambdaHist,convHist] = powerMethod(A,x0,tol,maxIt)
-    % Initialization
-    flag = 1;
-    [n,m] = size(A);
-    if n~= m, error('Algorithm works only for square matrices'); end
-    if nargin == 1
-        tol = 10^-6;
-        x0 = ones(n,1);
-        maxIt = 1000;
-    end
-    
-    % Computing 0-step
+    flag = 1;   
     x0 = x0/norm(x0);
     product = A*x0;
     lambdaHist(1) = x0' * product;
