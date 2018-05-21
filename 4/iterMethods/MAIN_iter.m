fa563a2824d32db2de47249e5cdc5fdab5f2d5d0
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2018-05-21 17:53:57 +0200

    other updates(3)

--- 4/iterMethods/MAIN_iter.m
+++ 4/iterMethods/MAIN_iter.m
@@ -17,8 +17,7 @@ x_reshape = reshape(x,N,N);
 surf(x_reshape, 'DisplayName', 'Solution');
 colormap(gray(50));
 title('Solution to Poisson');
-zlabel('Value');
-legend('Location', 'best');
+
 
 % REMAKR ON SPARSITY
 Q = gallery('poisson', 100);
