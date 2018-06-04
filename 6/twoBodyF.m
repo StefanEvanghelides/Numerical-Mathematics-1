c85ef1d25582b4a8b4b8dffe099d47970bba7398
FrankTN <f.g.te.nijenhuis@student.rug.nl>
2018-06-04 16:21:30 +0200

    Wrote an implementation for twoBody

--- 6/twoBodyF.m
+++ 6/twoBodyF.m
@@ -3,13 +3,12 @@
 % solVec    current solution (should be 6x1 array)
 % OUTPUT 
 % dSolVec   right-hand side
-function dSolVec = twoBodyF(t, solVec)
-    syms u(t) x(t)
+function dSolVec = twoBodyF(t, solVec) 
+    x = solVec(1:3);
+    u = solVec(4:6);
     
-    ode1 = diff(u) == - (4 * pi^2)/(norm(x,2)^3) * x;
-    ode2 = diff(x) == u;
-    odes = [ode1; ode2];
+    f1 = - (4*pi^2)/(norm(x,2)^3) * x;
+    f2 = u;
     
-    dSolVec = dsolve(odes);
-    
+    dSolVec = [ f1 f2 ];
 end
