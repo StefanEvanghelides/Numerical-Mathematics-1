3fdbec0cee19a4b8a11a1ba7286163f59db07876
Stefan Evanghelides <stefan.evanghelides@gmail.com>
2018-06-06 17:17:57 +0200

    small progress on part 1

--- 7/poissonSolveFD.m
+++ 7/poissonSolveFD.m
@@ -11,3 +11,40 @@
 % sol       (N+2) x 1 solution array
 % nodes     (N+2) x 1 array with location of spatial nodes
 function [sol, nodes] = poissonSolveFD(f, g, N, precon, tol)
+    nodes = zeros(N+2, 1);
+    b = zeros(N,1);
+    
+    h = 1/(N+1);
+    A = makeLaplace(N);
+    
+    nodes(1) = 0;
+    nodes(N+2) = 1;
+    for i = 1:N
+        b(i) = h^2 * f(i);
+        nodes(i+1) = i*h;
+    end
+    
+    dynamic = 2;
+    maxIt = 1000;
+    x0 = zeros(N,1);
+    
+    alpha = 0.5;
+    
+    lambda_max = eigs(A,1);
+    lambda_min = eigs(A,1,'SM');
+    alpha_opt = 2 / (lambda_max + lambda_min);
+    
+   
+    switch precon
+        case 'jacobi'
+            P = spdiags(diag(A), 0, size(A, 1), size(A, 2));
+        case 'gs'
+            P = tril(A, 0);
+        otherwise % for 'none'
+            P = [];
+    end
+    
+    [u, ~, ~] = iterMethod2(A,b,x0,tol,maxIt,P,dynamic,alpha);
+     
+    sol = [g(1); u; g(2)];
+end
